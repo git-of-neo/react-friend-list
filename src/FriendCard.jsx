8ba11f21f09c4501d7a44b453949a8dbebb4308c
Neo Shao Hong <64857033+git-of-neo@users.noreply.github.com>
2022-12-07 18:11:25 +0800

    general card styling

--- src/FriendCard.jsx
+++ src/FriendCard.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const FriendCard = ({ friend }) => {
     return (
-        <div>
-            <p>{friend.name.first}</p>
-            <p>{friend.name.last}</p>
-            <p>{friend.phone}</p>
-            <p>{friend.email}</p>
+        <div className="card-container">
+            <div className='card'>
+                <p>{friend.name.first}</p>
+                <p>{friend.name.last}</p>
+                <p>{friend.phone}</p>
+                <p>{friend.email}</p>
+                {/* additional info */}
+                <p>{friend.dob.date}</p>
+                <p>{`${friend.location.street.number}, ${friend.location.street.name}, ${friend.location.city}, ${friend.location.state}, ${friend.location.country} `}</p>
+            </div>
         </div>
     )
 }
