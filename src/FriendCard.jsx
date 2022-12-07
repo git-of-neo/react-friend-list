503416fc9e1c3f79489e7bb837c2bd8be4b64693
Neo Shao Hong <64857033+git-of-neo@users.noreply.github.com>
2022-12-07 18:44:23 +0800

    more general styling + images

--- src/FriendCard.jsx
+++ src/FriendCard.jsx
@@ -4,13 +4,16 @@ const FriendCard = ({ friend }) => {
     return (
         <div className="card-container">
             <div className='card'>
-                <p>{friend.name.first}</p>
-                <p>{friend.name.last}</p>
-                <p>{friend.phone}</p>
-                <p>{friend.email}</p>
-                {/* additional info */}
-                <p>{friend.dob.date}</p>
-                <p>{`${friend.location.street.number}, ${friend.location.street.name}, ${friend.location.city}, ${friend.location.state}, ${friend.location.country} `}</p>
+                <img src={friend.picture.medium}></img>
+                <div className="card__content">
+                    <p>{friend.name.first}</p>
+                    <p>{friend.name.last}</p>
+                    <p>{friend.phone}</p>
+                    <p>{friend.email}</p>
+                    {/* additional info */}
+                    <p>{friend.dob.date}</p>
+                    <p>{`${friend.location.street.number}, ${friend.location.street.name}, ${friend.location.city}, ${friend.location.state}, ${friend.location.country} `}</p>
+                </div>
             </div>
         </div>
     )
